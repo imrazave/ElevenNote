f83406dba0bb0424db6a3061233aa2697a0e7359
imrazave <imrazave@gmail.com>
2021-04-04 15:21:43 -0500

    7.02 Validation Messages

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -18,7 +18,7 @@ namespace ElevenNote.WebMVC
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
             var model = service.GetNotes();
-            
+
             return View(model);
         }
 
@@ -32,17 +32,26 @@ namespace ElevenNote.WebMVC
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
+            if (!ModelState.IsValid) return View(model);
 
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
+
+            ModelState.AddModelError("", "Note could not be created");
+
+            return View(model);
+        }
+
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
-
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
